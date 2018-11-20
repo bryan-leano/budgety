82e2b4c5bc6276b4ad876aea0801e994c5439e7a
Bryan Leanoe <bryan.leano@gmail.com>
2018-11-19 22:41:22 -0700

    add in first event listeners

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
-  var x = 23;
-
-  var add = function(a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
+  // some code
 
 })();
 
+
+// UI CONTROLLER
 var UIController = (function() {
 
   //some code
 
 })();
 
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var z = budgetCtrl.publicTest(5);
+  var ctrlAddIteam = function() {
+    // 1. Get the field input data
 
-  return {
-    anotherPublic: function(){
-      console.log(z);
-    }
+    // 2. Add the item to the budget controller
+
+    // 3. Add the new item to the user interface
+
+    // 4. Calculate budget
+
+    // 5. Display budget on the UI
+
+    console.log('It works!');
   }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddIteam);
+
+  document.addEventListener('keypress', function(event) {
+
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddIteam();
+
+    };
+
+  });
 
 })(budgetController, UIController);
