6bd057d86592b5bf71175018b61ea609f288336c
Bryan Leanoe <bryan.leano@gmail.com>
2018-11-20 23:44:14 -0700

    add new item to budget controller ... q q :q quit i > .

--- app.js
+++ app.js
@@ -13,6 +13,50 @@ var budgetController = (function() {
     this.value = value;
   };
 
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      expense: 0,
+      income: 0
+    }
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      
+    
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on 'inc' or 'exp' type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      };
+
+      // Push it into our data structure
+      data.allItems[type].push(newItem);
+
+      // Return new element
+      return newItem;
+    },
+
+    testing: function() {
+      console.log(data);
+    }
+  };
+
 })();
 
 // UI CONTROLLER
@@ -33,8 +77,9 @@ var UIController = (function() {
         value: document.querySelector(DOMstrings.inputValue).value
       };
     },
+  
 
-    getDOMstrings: function(){
+    getDOMstrings: function() {
       return DOMstrings;
     }
   };
@@ -48,23 +93,25 @@ var controller = (function(budgetCtrl, UICtrl) {
   var setupEventListeners = function() {
     var DOM = UICtrl.getDOMstrings();
     
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddIteam);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
     
       document.addEventListener('keypress', function(event) {
     
         if (event.keyCode === 13 || event.which === 13) {
-          ctrlAddIteam();
+          ctrlAddItem();
         };
       });
   };
 
 
-  var ctrlAddIteam = function() {
+  var ctrlAddItem = function() {
+    var input, newItem;
 
     // 1. Get the field input data
-    var input = UICtrl.getinput();
+    input = UICtrl.getinput();
 
     // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Add the new item to the user interface
 
