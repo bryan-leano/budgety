9e7d90e71ce2185e7ab66ab9a50dd1bb3addfae8
Bryan Leanoe <bryan.leano@gmail.com>
2018-11-28 14:34:55 -0700

    add updatePercentages function

--- app.js
+++ app.js
@@ -232,6 +232,16 @@ var controller = (function(budgetCtrl, UICtrl) {
     UICtrl.displayBudget(budget);
   };
 
+  var updatePercentages = function() {
+
+    // 1. Calculate percentages
+
+    // 2. Read percentages from the budget controller
+
+    // 3. Update the UI with the new percentages
+
+  };
+
   var ctrlAddItem = function() {
     var input, newItem;
 
@@ -248,8 +258,11 @@ var controller = (function(budgetCtrl, UICtrl) {
       // 4. Clear fields
       UICtrl.clearFields();
     
-      // 5 . Calculate and update budget
+      // 5. Calculate and update budget
       updateBudget();
+
+      // 6. Calculate and update percentages
+      updatePercentages();
     }
 
   };
@@ -274,6 +287,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
       // 3. Update and show the new budget
       updateBudget();
+
+      // 4. Calculate and update percentages
+      updatePercentages();
     }
 
   };
